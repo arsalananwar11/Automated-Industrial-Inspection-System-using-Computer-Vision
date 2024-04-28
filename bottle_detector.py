59bc11cacc040cc09225feba83d2816b71d9a505
Arsalan Anwar <arsalan.anwar.7777@gmail.com>
2024-04-28 14:39:59 -0400

    Added edge detection on focus video segment

--- bottle_detector.py
+++ bottle_detector.py
@@ -5,25 +5,46 @@ class BottleDetector:
   def __init__(self, redundancy_range = 10):
       self.redundancy_range = redundancy_range
 
-  def get_focus_segment(self, frame, top_left = (125, 500), bottom_right = (375, 1100)):
-          blurred_img = cv2.GaussianBlur(frame, (35, 35), 0)
-          mask = np.zeros_like(frame, dtype=np.uint8)
-          mask = cv2.rectangle(mask, top_left, bottom_right, (255, 255, 255), -1)
-          focus_segment = np.where(mask==(255, 255, 255), frame, blurred_img)
-          return focus_segment
+  def perform_frame_blurring(self, frame, ksize=(35,35)):
+    blurred_frame = cv2.GaussianBlur(frame, (35, 35), 0)
+    return blurred_frame
 
-  def draw_rectangle_and_text(self, frame, type="bottle", top_left = (125, 500), bottom_right = (375, 1100)):
-    if type!="bottle":
-      text="Area of Focus"
-      rectangle_color = (0, 0, 255)
-      rectangle_border_thickness = 8
-      text_weight = 3
+  def get_focus_segment(self, original_frame, blurred_frame, top_left = (125, 500), bottom_right = (375, 1100)):
+    mask = np.zeros_like(original_frame, dtype=np.uint8)
+    mask = cv2.rectangle(mask, top_left, bottom_right, (255, 255, 255), -1)
+    focus_segment = np.where(mask==(255, 255, 255), original_frame, blurred_frame)
+    return focus_segment
+  
+  def edge_detection(self, frame):
+    edges = cv2.Canny(frame, 100, 200)
+    edges = cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)   
+    return edges
 
-    else:
-      text = "Bottle Detected"
-      rectangle_color = (0, 255, 0)
-      rectangle_border_thickness = 3
-      text_weight = 2
+  def perform_edge_detection_on_focus_segment(self, frame, blurRestOfFrame, top_left=(125, 500), bottom_right=(375, 1100)):
+    mask = np.zeros_like(frame, dtype=np.uint8)
+    mask = cv2.rectangle(mask, top_left, bottom_right, (255, 255, 255), -1)
+    edges = self.edge_detection(frame)
+    if blurRestOfFrame:
+      frame = cv2.GaussianBlur(frame, (35, 35), 0)
+    
+    focus_segment = np.where(mask == (255, 255, 255), edges, frame) 
+    return focus_segment
+
+  def draw_focus_area_and_text(self, frame, text, top_left = (125, 500), bottom_right = (375, 1100)):
+    rectangle_color = (0, 0, 255)
+    rectangle_border_thickness = 8
+    text_weight = 3
+    cv2.rectangle(frame, top_left, bottom_right, rectangle_color, rectangle_border_thickness)  
+    font = cv2.FONT_HERSHEY_SIMPLEX 
+    text_size, _ = cv2.getTextSize(text, font, 1, 2)  
+    text_x = top_left[0] + 12
+    text_y = top_left[1] - 12
+    cv2.putText(frame, text, (text_x, text_y), font, 1, (240, 255, 235), text_weight)
+
+  def draw_box_and_text(self, frame, text="Bottle Detected", top_left = (125, 500), bottom_right = (375, 1100)):
+    rectangle_color = (0, 255, 0)
+    rectangle_border_thickness = 3
+    text_weight = 2
     cv2.rectangle(frame, top_left, bottom_right, rectangle_color, rectangle_border_thickness)  
     font = cv2.FONT_HERSHEY_SIMPLEX 
     text_size, _ = cv2.getTextSize(text, font, 1, 2)  
